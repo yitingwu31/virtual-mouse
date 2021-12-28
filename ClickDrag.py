0361c00010184c8df1dbfc5e2af65f52ec0a4bbb
yitingwu31 <animalfamily31@gmail.com>
2021-12-28 14:02:20 +0800

    implement autopy mouse

--- ClickDrag.py
+++ ClickDrag.py
@@ -2,9 +2,11 @@ import cv2
 import HandTrackingModule as htm
 import numpy as np
 import autopy
+import time
 
 wCam, hCam = 640, 480
 wScr, hScr = autopy.screen.size() # 1440, 900
+wframeR, hframeR = 100, 50 # reduction window
 
 def findHandGesture(count, finger_statuses, lmlists):
     hand_gestures = {'RIGHT': "Unknown", 'LEFT': "Unknown"}
@@ -50,11 +52,22 @@ def recoMode(hand_gestures):
     return mode
 
 def moveCursor(x1, y1):
-    x3 = np.interp(x1, (0, wCam), (0, wScr))
-    y3 = np.interp(y1, (9, hCam), (0, hScr))
+    x3 = np.interp(x1, (wframeR, wCam - wframeR), (0, wScr))
+    y3 = np.interp(y1, (hframeR, hCam - 3 * hframeR), (0, hScr))
+    if x3 >= wScr:
+        x3 = wScr
+    elif x3 <= 0:
+        x3 = 0
+    if y3 >= hScr:
+        y3 = hScr
+    elif y3 <= 0:
+        y3 = 0
 
     autopy.mouse.move(x3, y3)
 
+# def mouseClick(ptime, ctime, clickFlag):
+#     if clickFlag:
+#         if ctime - ptime 
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -62,7 +75,13 @@ def main():
     cap.set(4, hCam)
     detector = htm.handDetector(detectionCon=0.7)
 
-    image1 = cv2.imread('smallotter.png')
+    smoothening = 5
+    pX, pY = 0, 0   # previous location
+    cX, cY = 0, 0   # current location
+
+    # filtering for consecutive move
+    ptime, ctime = 0, 0
+    clickFlag = False
 
     while True:
         successs, img = cap.read()
@@ -76,6 +95,7 @@ def main():
             lmlists = detector.findTwoHandPositions(frame, draw=False)
             # print(lmlists)
             # print(len(lmlists['RIGHT']), len(lmlists['LEFT']))
+            cv2.rectangle(frame, (wframeR, hframeR), (wCam - wframeR, hCam - 3 * hframeR), (255, 0, 255), 2)
             
             hand_gestures = findHandGesture(count, finger_statuses, lmlists)
             mode = recoMode(hand_gestures)
@@ -83,7 +103,16 @@ def main():
                 x1 = lmlists['RIGHT'][8][1]
                 y1 = lmlists["RIGHT"][8][2]
                 cv2.circle(frame, (x1, y1), 7, (127, 255, 0), cv2.FILLED)
-                moveCursor(x1, y1)
+                cX = pX + (x1 - pX) / smoothening
+                cY = pY + (y1 - pY) / smoothening
+                moveCursor(cX, cY)
+                pX, pY = cX, cY
+            if mode == "Click":
+                x1 = lmlists['RIGHT'][8][1]
+                y1 = lmlists['RIGHT'][8][2]
+                cv2.circle(frame, (x1, y1), 10, (0, 22, 144), 3)
+                # mouseClick(ptime, ctime, clickFlag)
+                autopy.mouse.click()
 
         cv2.imshow('Virtual Mouse', frame)
 
